2836f645d399b2179e73ba17ca309bbbd2696683
itphihv1 <itphihv1@ghtk.co>
2024-12-30 23:44:04 +0700

    init

--- repo/routes/web.php
+++ repo/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Welcome to Laravel';
+    // Kiểm tra xem 'welcome_message' có trong cache không
+    $welcomeMessage = Cache::get('welcome_message');
+
+    // Nếu không có trong cache, thiết lập giá trị và lưu vào cache
+    if (!$welcomeMessage) {
+        dd(1);
+        $welcomeMessage = 'Welcome to Laravel';
+        Cache::put('welcome_message', $welcomeMessage, 600); // Lưu trữ trong 10 phút (600 giây)
+    }
+
+    return $welcomeMessage;
 });
